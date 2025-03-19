ca13b84c2dce47aa53f9c83fe4fc9c019ec0f171
alfianekrhmn <alfian.eka481@gmail.com>
2025-03-19 21:32:35 +0700

    feat: menambahkan contoh variabel dan tipe data di JavaScript

---
+++ task2.js
@@ -0,0 +1,9 @@
+let name = "Alfian"
+let age = 21
+let height = 164.5
+let stillLearningJavascript = true
+
+console.log(`My name's ${name}`)
+console.log(`I'm ${age} years old`)
+console.log(`I have a height of ${height} cm`)
+console.log(`are you still learning Javascript? ${stillLearningJavascript}`)
